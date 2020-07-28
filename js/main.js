88d197b265ef73f8d8e693188b27633026baa6ea
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-28 17:30:30 +0300

    add normal click

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Create Board
     function createBoard() {
-    	// get shuffled game array with random bombs
-    	const bombsArray = Array(bombAmount).fill('bomb')
-    	const emptyArray = Array(width*width - bombAmount).fill('valid')
-    	const gameArray = emptyArray.concat(bombsArray)
-    	const shuffledArray = gameArray.sort(() => Math.random() -0.5)
-    	
+        // get shuffled game array with random bombs
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width * width - bombAmount).fill('valid')
+        const gameArray = emptyArray.concat(bombsArray)
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
+
 
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
@@ -19,22 +19,31 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
+
+            //normal click
+            square.addEventListener('click', function(e){
+            	click(square)
+            })
         }
 
         // add numbers
         for (let i = 0; i < squares.length; i++) {
-        	let total = 0
-        	const isLeftEdge = (1 % width === 0)
-        	const isRightEdge = (1 % width === width -1)
+            let total = 0
+            const isLeftEdge = (1 % width === 0)
+            const isRightEdge = (1 % width === width - 1)
 
-        	if(squares[i].classList.contains('valid')){
-        		if(i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
-        	}
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
+                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total ++
+                if (i > 10 && squares[i - width].classList.contains('bomb')) total ++
+                if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total ++
+                if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total ++	
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total ++
+                if (i < 89 && squares[i + width].classList.contains('bomb')) total ++	
+                squares[i].setAttribute('data', total)	
+            }
         }
-
-
-
     }
     createBoard()
-
 })
