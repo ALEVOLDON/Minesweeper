54d9641cce271d3d92af5fc61d03ffb19bcad609
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-28 17:50:33 +0300

    add click on square actions

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
 
             //normal click
-            square.addEventListener('click', function(e){
-            	click(square)
+            square.addEventListener('click', function(e) {
+                click(square)
             })
         }
 
@@ -34,16 +34,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (squares[i].classList.contains('valid')) {
                 if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total++
-                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total ++
-                if (i > 10 && squares[i - width].classList.contains('bomb')) total ++
-                if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total ++
-                if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total ++	
-                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++
-                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total ++
-                if (i < 89 && squares[i + width].classList.contains('bomb')) total ++	
-                squares[i].setAttribute('data', total)	
+                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++
+                if (i > 10 && squares[i - width].classList.contains('bomb')) total++
+                if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++
+                if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) total++
+                if (i < 89 && squares[i + width].classList.contains('bomb')) total++
+                squares[i].setAttribute('data', total)
             }
         }
     }
     createBoard()
+
+    //click on square actions
+    function click(square) {
+        if (square.classList.contains('bomb')) {
+            console.log('Game over')
+        } else {
+            let total = square.getAttribute('data')
+            if (total != 0) {
+                square.classList.add('checked')
+            }
+        }
+    }
 })
