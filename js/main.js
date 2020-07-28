bedc1716d51c2c7189ace2e98821f736a83c1e6c
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-28 16:51:09 +0300

    add responsive

--- js/main.js
+++ js/main.js
@@ -8,17 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
     	// get shuffled game array with random bombs
     	const bombsArray = Array(bombAmount).fill('bomb')
-    	console.log(bombsArray)
     	const emptyArray = Array(width*width - bombAmount).fill('valid')
-    	console.log(emptyArray)
     	const gameArray = emptyArray.concat(bombsArray)
-    	console.log(gameArray)
     	const shuffledArray = gameArray.sort(() => Math.random() -0.5)
-    	console.log(shuffledArray)
+    	
 
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
         }
