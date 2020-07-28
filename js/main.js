23886dcbe652c13cbceb9db6392a0bd5c85b69e6
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-28 21:51:28 +0300

    cntrl and left click

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let width = 10
     let bombAmount = 20
+    let flags = 0
     let squares = []
     let isGameOver = false
 
@@ -47,6 +48,22 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     createBoard()
+
+    //add flag with right click
+    function addFlag(square) {
+    	if (isGameOver) return
+   		if (!square.classList.contains('checked') && (flags < bombAmount)) {
+   			if (!square.classList.contains('flag')) {
+   				square.classList.add('flag')
+   				square.innerHTML = '🚩'
+   				flags ++
+   			} else {
+   			  square.classList.remove('flag')
+   			  square.innerHTML = ''
+   			  flags --	
+   			}
+   		}
+    }
 
     //click on square actions
     function click(square) {
