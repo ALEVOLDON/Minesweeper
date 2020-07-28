32a5509eccf670e605ed2cb34c2aec0767425782
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-28 16:27:45 +0300

    update filse

--- js/main.js
+++ js/main.js
@@ -8,9 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
     	// get shuffled game array with random bombs
     	const bombsArray = Array(bombAmount).fill('bomb')
-    	const emptyArray = Array(width*width - bombAmount).fill('valid')
     	console.log(bombsArray)
-    	console.log(emptyArray)	
+    	const emptyArray = Array(width*width - bombAmount).fill('valid')
+    	console.log(emptyArray)
+    	const gameArray = emptyArray.concat(bombsArray)
+    	console.log(gameArray)
+    	const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+    	console.log(shuffledArray)
 
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
